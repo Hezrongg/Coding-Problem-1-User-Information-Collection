f7a0f65725d84aed90b7d8bc576f5adbf96704d6
Hezrongg <hezrongg26@gmail.com>
2024-11-04 17:42:54 +0800

    initial commit

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let fullName = "Hezron Raboc Gagarin";
+let age = "19";
+let favoriteNumber = "26";
+let favoriteColor = "Black";
+
+console.log(fullName);
+console.log(age);
+console.log(favoriteNumber);
+console.log(favoriteColor);
